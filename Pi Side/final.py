977bfcabf9e98f74e2f6f8aad0e270e25ff5a0ee
Robert Beckwith <rjbeckwith55@gmail.com>
2017-04-27 18:04:17 -0500

    Update final.py

--- Pi Side/final.py
+++ Pi Side/final.py
@@ -89,6 +89,18 @@ while True:
 		else:
 			Convey.ChangeDutyCycle(50)
 			print "Conveyor Off"
+	if info[:2] == 'QU'
+		print info
+		Drive.ChangeDutyCycle(50)
+		Convey.ChangeDutyCycle(50)
+		Steer.ChangeDutyCycle(50)
+		Tilt.ChangeDutyCycle(50)
+		Auger.ChangeDutyCycle(50)
+		Slide.ChangeDutyCycle(50)
+		print "Clean Shutdown"
+		break
+		
+		
 			
 s.close()
 exit(0)
