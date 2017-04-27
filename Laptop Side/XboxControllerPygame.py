581b89d8dd89a37c2aab1f6306f32293e4aaac85
Sam Ciucci <sciu0482@gmail.com>
2017-04-26 19:17:47 -0500

    updated

--- Laptop Side/XboxControllerPygame.py
+++ Laptop Side/XboxControllerPygame.py
@@ -13,10 +13,12 @@ import pygame
 	"""
 
 def stop():
+        msg = "DR" + 50.0
+        s.send(msg)
         pygame.joystick.quit()
         pygame.quit()
         print("Clean exit")
-        exit(0)
+        
 
 def main():
     print("main")
@@ -42,23 +44,31 @@ def main():
     print("joystick")
     #initailize joystick
     joystick.init()
+    pwmst = 50.0
+    pwmdr = 50.0
     
     while(running):
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYAXISMOTION:
-
+                
                 #Steering command on the top left axis left and right direction
                 if joystick.get_axis(0) > .1 or joystick.get_axis(0) < -.1:
-                    pwm = (joystick.get_axis(0) + 1) / .02
-                    pwm = round(pwm,0)
-                    if pwm > 90:
-                        pwm = 90.0
-                    elif pwm < 10:
-                        pwm = 10.0
-                    msg = "ST" + str(pwm)
+                    pwmst = (joystick.get_axis(0) + 1) / .02
+                    pwmst = round(pwmst,0)
+                    if pwmst > 90:
+                        pwmst = 90.0
+                    elif pwmst < 10:
+                        pwmst = 10.0
+                    msg = "ST" + str(pwmst)
                     print(msg)
                     data = msg.encode()
                     s.send(data)
+                elif joystick.get_axis(0) <.1 and joystick.get_axis(0)>-.1 and pwmst!=50.0:
+                    pwmst = 50.0
+                    msg = "ST" + str(pwmst)
+                    print(msg)
+                    send = msg.encode()
+                    s.send(send)
 
                 #Conveyor Belt control using the X button
                 if joystick.get_button(2) != 0:
@@ -68,7 +78,7 @@ def main():
                     data = msg.encode()
                     s.send(data)
 
-                #Auger control using the A button
+               #Auger control using the A button
                 if joystick.get_button(0) != 0:
                     pwm = 10.0
                     msg = "AU" + str(pwm)
@@ -85,8 +95,14 @@ def main():
                     s.send(send)
                 
                 #Ballscrew slide using the left trigger
-                if joystick.get_axis(2) > .5:
-                    pwm = 70
+                if joystick.get_axis(2) > .1:
+                    pwm = 70.0
+                    msg = "SL" + str(pwm)
+                    print(msg)
+                    send = msg.encode()
+                    s.send(send)
+                elif joystick.get_axis(2) < .1 and pwm == 70.0:
+                    pwm = 50.0
                     msg = "SL" + str(pwm)
                     print(msg)
                     send = msg.encode()
@@ -94,7 +110,13 @@ def main():
 
                 #Ballscrew slide using right trigger
                 if joystick.get_axis(2) < -.5:
-                    pwm = 30
+                    pwm = 30.0
+                    msg = "SL" + str(pwm)
+                    print(msg)
+                    send = msg.encode()
+                    s.send(send)
+                elif joystick.get_axis(2) < -.1 and pwm == 30.0:
+                    pwm = 50.0
                     msg = "SL" + str(pwm)
                     print(msg)
                     send = msg.encode()
@@ -118,13 +140,19 @@ def main():
                 
                 #Drive command using top left stick up and down
                 if joystick.get_axis(1) <-.1 or joystick.get_axis(1) > .1:
-                    pwm = (joystick.get_axis(1) + 1) / .02      
-                    pwm = round(pwm,0)
-                    if pwm > 90:
-                        pwm = 90.0
-                    elif pwm < 10:
-                        pwm = 10.0
-                    msg = "DR" + str(pwm)
+                    pwmdr = (joystick.get_axis(1)+1) / .02      
+                    pwmdr = round(pwmdr,0)
+                    if pwmdr > 90:
+                        pwmdr = 90.0
+                    elif pwmdr < 10:
+                        pwmdr = 10.0
+                    msg = "DR" + str(pwmdr)
+                    print(msg)
+                    send = msg.encode()
+                    s.send(send)
+                elif joystick.get_axis(1) < .1 and joystick.get_axis(1) >-.1 and pwmdr!=50.0:
+                    pwmdr = 50.0
+                    msg = "DR" + str(pwmdr)
                     print(msg)
                     send = msg.encode()
                     s.send(send)
